7427f631bd80c9eef7c07c0fd91fd7cbb79b6921
8e7 <justinlai03@yahoo.com.tw>
2023-10-07 18:46:41 +0800

    fixed music.mjs

--- app/library/music.mjs
+++ app/library/music.mjs
@@ -4,26 +4,19 @@ import * as Tone from 'tone'
 
 //const synth = new Tone.Synth().toDestination();
 
-const synth = new Tone.PolySynth(3, Tone.Synth, {
-    oscillator : {
-      type : "sawtooth"
-    }
-  }).toDestination();
+//TODO: instantiate the synth into useeffect
+const synth = new Tone.PolySynth().toDestination();
 
 const notes = [
     { pitch: "C4", timing: 0 },
-    { pitch: "D4", timing: 0 },
+    //{ pitch: "D4", timing: 0 },
     { pitch: "E4", timing: 0 },
-    { pitch: "F4", timing: 0 },
+    //{ pitch: "F4", timing: 0 },
     { pitch: "G4", timing: 0 }
 ];
 
 export function play_music() {
-    let delay = Tone.now();
-    for(let i = 0; i < notes.length; i++) {
-        delay += notes[i].timing;
-        synth.triggerAttackRelease(notes[i].pitch, '8n', delay);  
-    }
+    synth.triggerAttackRelease(["C4", "E4", "A4"], 1);
 }
 
 //play() // call this when someone interacts with your program.
