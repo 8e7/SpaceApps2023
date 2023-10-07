82aec2be16ee1d9f143b2f41137dfed1594a806f
8e7 <justinlai03@yahoo.com.tw>
2023-10-07 13:15:27 +0800

    Cleaned template

---
+++ app/page.js
@@ -0,0 +1,11 @@
+"use client";
+import { useState } from 'react'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
