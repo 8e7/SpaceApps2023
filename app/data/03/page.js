f9f97e2c5b0d42e7d175d855e1e2978aa9d398a4
JoyLiao0211 <liaojoy985@gmail.com>
2023-10-08 21:58:08 +0800

    minor upd on description

--- app/data/03/page.js
+++ app/data/03/page.js
@@ -21,7 +21,7 @@ export default function Home() {
     <div id="leftpart">
       <SpaceImage />
       <div id="section">
-        <h3>About M1</h3>
+        <h3>About M1 Crab Nebula</h3>
         <p>
         M1, also known as Crab Nebula, is a supernova remanat with a pulsar star at the center. Its central pulsar star keeps emitting high-energy gamma ray and in 2019, we received phtons with energy higher than 100TeV. The Crab Nebula is currently expanding outward at the speed of 1500km/s and studies of it help us get a better understanding of the dynamics of supernova remanat.
         </p>
