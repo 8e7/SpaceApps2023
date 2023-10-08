0166d7849f07c1e69b678024c803accfc942238b
8e7 <justinlai03@yahoo.com.tw>
2023-10-08 13:26:43 +0800

    Added intensity and read correct file

--- app/library/music.mjs
+++ app/library/music.mjs
@@ -15,7 +15,7 @@ const root_notes = ["C2", "G3", "D4", "A4"];
 const notes = [
 
 ];
-export function play_path(synths, path, grid, gridSize) {
+export function play_path(synths, path, grid, gridSize, speed) {
     let len = path.length;
     if (len === 0) {
         return;
@@ -30,16 +30,21 @@ export function play_path(synths, path, grid, gridSize) {
         Tone.Transport.bpm.value = new_bpm;
     };
 
+    
     for (let i = 0;i < len;i++) {
         let [x, y] = path[i];
+        /* change bpm
         if (i) {
             const bpm_multiplier = 2;
             let speed = Math.hypot(path[i][0]-path[i-1][0], path[i][1]-path[i-1][1]);
             Tone.Transport.scheduleOnce(change_bpm(50+speed*bpm_multiplier), total_time);
-            //change_bpm.start(total_time); 
         }
+        */
         
-        let wave = grid[x][y];
+        let wave = grid[x][y].slice(0, 4);
+        let intensity = grid[x][y][4];
+        
+
         const maxValue = Math.max(...wave);
         const maxInd = wave.indexOf(maxValue);
         Tone.Transport.scheduleOnce(() =>{synths[maxInd].triggerAttackRelease(root_notes[maxInd], "8n")}, total_time);
