6551feaa78993edb9844c1724205ceee7e76ede2
8e7 <justinlai03@yahoo.com.tw>
2023-10-08 11:50:15 +0800

    added bpm changing

--- app/library/music.mjs
+++ app/library/music.mjs
@@ -7,32 +7,42 @@ import * as Tone from 'tone'
 //TODO: instantiate the synth into useeffect
 
 
-const notes = [
-    { pitch: "C4", timing: 0 },
-    { pitch: "E4", timing: 0 },
-    { pitch: "G4", timing: 0 }
-];
-
-
 export function play_music(synth) {
     synth.triggerAttackRelease(["C4", "E4", "A4"], 1);
 }
 
 const root_notes = ["C2", "G3", "D4", "A4"];
+const notes = [
+
+];
 export function play_path(synths, path, grid, gridSize) {
     let len = path.length;
     if (len === 0) {
         return;
     }
+    console.log("Playing");
     Tone.Transport.stop();
+    Tone.Transport.cancel();
     let [n, m] = gridSize;
     let total_time = 0;
+    
+    let change_bpm = (new_bpm) =>{
+        Tone.Transport.bpm.value = new_bpm;
+    };
+
     for (let i = 0;i < len;i++) {
         let [x, y] = path[i];
+        if (i) {
+            const bpm_multiplier = 2;
+            let speed = Math.hypot(path[i][0]-path[i-1][0], path[i][1]-path[i-1][1]);
+            Tone.Transport.scheduleOnce(change_bpm(50+speed*bpm_multiplier), total_time);
+            //change_bpm.start(total_time); 
+        }
+        
         let wave = grid[x][y];
         const maxValue = Math.max(...wave);
         const maxInd = wave.indexOf(maxValue);
-        Tone.Transport.scheduleOnce(() =>{synths[maxInd].triggerAttackRelease(root_notes[maxInd], "8n")}, total_time); 
+        Tone.Transport.scheduleOnce(() =>{synths[maxInd].triggerAttackRelease(root_notes[maxInd], "8n")}, total_time);
         total_time += Tone.Time("8n").toSeconds();
     }
     Tone.Transport.start();
